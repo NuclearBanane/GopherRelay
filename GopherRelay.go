9e0cc12fa3d607c8206927703dc2a49d9c9a3423
NuclearBanane <rwald@uocsclub.ca>
2014-12-25 00:44:28 -0600

    Hello world! Changes in the readme.

---
+++ GopherRelay.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("I am a Gopher Relay Bot\n")
+	fmt.Println("Not today but maybe some day soon...")
+}
--- README.md
+++ README.md
@@ -2,3 +2,9 @@ GopherRelay
 ===========
 
 GoLang IRC Bot
+
+Work in progress. 
+
+Currently just a `hello world`... 
+While I get used to my new setup
+
